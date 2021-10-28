e04b98b5713d08862e0faff048463d0cc349afff
tamir <tamir@troi.co.il>
2021-10-28 11:03:00 +0300

    car component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-//import logo from './logo.svg';
 import React, { Component } from 'react';
-//import ReactDOM from 'react-dom';
+import Car from './components/Car';
+
 
 class App extends Component {
 	render() {
 		return (
       <div className="App">
         <header className="App-header">
-			<p>Hello World</p>
+          <p>Hello World</p>
+					<Car />
+					<hr />
+					<Car />
+					<hr />
         </header>
       </div>
     );
---
+++ src/components/Car.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+
+class Car extends Component {
+  render() {
+    return (
+      <div>
+        <p>Brand: Honda</p>
+        <p>Model: Civic</p>
+        <p>Color: Black</p>
+      </div>
+    );
+  }
+}
+
+export default Car;
