b2b95bf647b29fef8a323d2ccca836db3a0507fa
tamir <tamir@troi.co.il>
2021-11-01 20:40:03 +0200

    delete

--- src/App.js
+++ src/App.js
@@ -4,34 +4,44 @@ import Garage from "./components/Garage";
 import AddCar from "./components/AddCar";
 
 class App extends Component {
-	static my_car_seq = 4;
+  static my_car_seq = 4;
   state = {
     my_cars: [
-      {brand: "Honda", model: "Civic", color: "Black", year: 2018, id: 1},
-      {brand: "Toyota", model: "Kamry", color: "White", year: 2019, id: 2},
-      {brand: "Ferrari", model: "Testa rocca", color: "Red", year: 2020, id: 3}
+      { brand: "Honda", model: "Civic", color: "Black", year: 2018, id: 1 },
+      { brand: "Toyota", model: "Kamry", color: "White", year: 2019, id: 2 },
+      {
+        brand: "Ferrari",
+        model: "Testa rocca",
+        color: "Red",
+        year: 2020,
+        id: 3,
+      },
     ],
   };
 
-	addCar = (new_car) => {
-		const new_arr = [...this.state.my_cars]
-		new_arr.push({...new_car, id: App.my_car_seq++ });
-		this.setState(
-			{
-			my_cars: new_arr
-			}
-		)
-
-	}
+  addCar = new_car => {
+    const new_arr = [...this.state.my_cars];
+    new_arr.push({ ...new_car, id: App.my_car_seq++ });
+    this.setState({
+      my_cars: new_arr,
+    });
+	};
 	
+	deleteCar = (_id) => {
+		const new_cars = this.state.my_cars.filter(car => car.id !== _id)
+		this.setState({
+			my_cars: new_cars
+		})
+	}
+
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ marginLeft: 10 }}>
         <header className="App-header">
-				 <p>Garage!</p>
-				 <hr />
-				 <Garage cars={this.state.my_cars} />
-				 <AddCar addCar={this.addCar}/>
+          <p>Garage!</p>
+          <hr />
+				 <Garage cars={this.state.my_cars} delete_car={this.deleteCar}/>
+          <AddCar addCar={this.addCar} />
         </header>
       </div>
     );
--- src/components/Garage.js
+++ src/components/Garage.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 
 class Garage extends Component {
-
   render() {
     console.log(this.props);
-    const { cars } = this.props;
+	  const { cars } = this.props;
+	  const {delete_car} = this.props
     const cars_list = cars.map(_car => {
       return _car.year > 2017 ? (
         <div key={_car.id}>
           <p>Brand: {_car.brand}</p>
           <p>Model: {_car.model}</p>
           <p>Color: {_car.color}</p>
+				<button style={{ background: 'yellow' }}
+				onClick={() => delete_car(_car.id)}>X</button>
+
           <hr />
-        </div>): null });
+        </div>
+      ) : null;
+    });
     return <div>{cars_list}</div>;
   }
 }
