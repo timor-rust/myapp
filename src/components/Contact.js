6836c7cd3e3cb8ab9cc2aac2a7b6d9da8393baba
tamir <tamir@troi.co.il>
2021-11-18 13:43:52 +0200

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 const Contact = props => {
-  console.log(props);
+	console.log(props);
+	console.log(props.history);
+	setTimeout(() => {
+		props.history.push('/about');
+	}, 3000)
 
   return (
     <div>
       <div className="container">
-        <h4 className="center">Contact</h4>
+			  <h4 className="center">Contact</h4>
+			  <p>3...2...1... about</p>
         <p>
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
